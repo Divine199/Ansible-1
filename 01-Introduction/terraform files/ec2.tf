2a71ca8c766ecbfd659b173bfeb32a27a28e6314
Divine199 <88943399+Divine199@users.noreply.github.com>
2021-12-01 08:12:02 -0600

    Update ec2.tf

--- 01-Introduction/terraform files/ec2.tf
+++ 01-Introduction/terraform files/ec2.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "ubuntu-hosts" {
   }
 }
 
-resource "aws_instance" "rhel_hosts" {
+resource "aws_instance" "rhel-hosts" {
   ami = data.aws_ami.rhel.id
   instance_type = var.my_instance_type
   user_data = file("${path.module}/create_ansible_user.sh")
